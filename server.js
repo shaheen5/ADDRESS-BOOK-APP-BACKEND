5e0a2c05bcbcd94e3c73aa65b25e68398c2e8149
shaheen5 <miyashaheen1995@gmail.com>
2021-06-29 13:56:12 +0530

    feat : create server using express & add body-parser middlewares

---
+++ server.js
@@ -0,0 +1,36 @@
+/**********************************************************************************************************
+ *  Execution    : 1. Default node with npm   cmd> npm server.js
+                   2. If nodemon installed    cmd> nodemon start
+ *
+ * Purpose      : entry point for the program where express app is created
+ *
+ * @description  :Dependencies require to be installed before execution of this file 
+ *
+ * @file        : server.js
+ * @overview    : Create web application using express,Set up the server,connect to database,define routes
+ * @module      : starting point to run the AddressBook API
+ * @author      : Shaheen M.
+ * @version     : 1.0
+ * @since       : 15-06-2021
+ **********************************************************************************************************/
+
+const express = require('express');
+require('dotenv').config();
+
+//create express app
+const app = express();
+
+//parse requests of content-type application/x-www-form-urlencoded
+app.use(express.urlencoded({extended:true}))
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+//define a simple route
+app.get('/',(req,res)=>{
+    res.json({"message" : "Welcome To Address Book Application"});
+});
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is listening at port ${process.env.PORT}`);
+});
